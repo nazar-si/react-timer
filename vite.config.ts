560d65b8005da8568c22daa94dd5b56b134356e5
nazar-si <nazari61@yandex.ru>
2023-07-11 09:06:53 +0300

    pwa: stoped pwa support for some time due to caching issues

--- vite.config.ts
+++ vite.config.ts
@@ -6,41 +6,6 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-      manifest: {
-        theme_color: '#f69435',
-        background_color: '#f69435',
-        display: 'browser',
-        scope: '/',
-        start_url: '/',
-        name: 'Pomodoro Timer',
-        short_name: 'Pomodoro',
-        description:
-          'Simple pomodoro timer for creating a more productive work environment',
-        icons: [
-          {
-            src: '/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/icon-256x256.png',
-            sizes: '256x256',
-            type: 'image/png',
-          },
-          {
-            src: '/icon-384x384.png',
-            sizes: '384x384',
-            type: 'image/png',
-          },
-          {
-            src: '/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
+    // VitePWA({}), // Temporary stoped PWA support
   ],
 });
