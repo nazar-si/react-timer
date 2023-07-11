fb83d33c5a2724a9dc293470fa49a2f98e12d1db
nazar-si <nazari61@yandex.ru>
2023-07-11 23:25:53 +0300

    test: fixed test giving error all the time due to wrong query

--- src/components/ui/ClockInput/__test__/ClockInput.test.tsx
+++ src/components/ui/ClockInput/__test__/ClockInput.test.tsx
@@ -3,14 +3,8 @@ import ClockInput from "../ClockInput";
 
 describe('ClockInput', ()=>{
     it("should render", ()=>{
-        render(<ClockInput value={12 * 60 + 34}/>)
-        const first = screen.getByText('1');
-        const second = screen.getByText('2');
-        const third = screen.getByText('3');
-        const fourth = screen.getByText('4');
-        expect(first).toBeInTheDocument();
-        expect(second).toBeInTheDocument();
-        expect(third).toBeInTheDocument();
-        expect(fourth).toBeInTheDocument();
+        render(<ClockInput value={0} setValue={()=>0}/>)
+        const dials = screen.getAllByText("0");
+        expect(dials.length).toBe(4);
     })
 })
