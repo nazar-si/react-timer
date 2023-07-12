88bb7ed6e19b1aca10f2c67332193e32f5657308
nazar-si <nazari61@yandex.ru>
2023-07-12 18:32:04 +0300

    feat: added settings translation for english

--- src/components/MainTimer/Settings/Settings.tsx
+++ src/components/MainTimer/Settings/Settings.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import Select from '../../ui/Select/Select';
 import ClockInput from '../../ui/ClockInput/ClockInput';
-import Button from '../../ui/Button/Button';
 import useTimerStore from '../../../store/timer/timer';
+import { useTranslation } from 'react-i18next';
 
 type Props = {};
 
@@ -12,14 +11,15 @@ export default function Settings({}: Props) {
   const [focusTime, setFocusTime] = useState(useTimerStore(s=>s.duration.focus));
   const [breakTime, setBreakTime] = useState(useTimerStore(s=>s.duration.break));
   const [longBreakTime, setLongBreakTime] = useState(useTimerStore(s=>s.duration.longBreak));
+  const {t} = useTranslation();
   return (
     <div>
-      <div className='text-xl font-bold -mt-1'>Settings</div>
-      <div className='text-lg font-medium mt-3 -mb-8'>Focus duration:</div>
+      <div className='text-xl font-bold -mt-1'>{t('settings.title')}</div>
+      <div className='text-lg font-medium mt-3 -mb-8'>{t("settings.duration-focus")}</div>
       <ClockInput value={focusTime} setValue={setFocusTime}/>
-      <div className='text-lg font-medium mt-3 -mb-8'>Break duration:</div>
+      <div className='text-lg font-medium mt-3 -mb-8'>{t("settings.duration-break")}</div>
       <ClockInput value={breakTime} setValue={setBreakTime}/>
-      <div className='text-lg font-medium mt-3 -mb-8'>Long break duration:</div>
+      <div className='text-lg font-medium mt-3 -mb-8'>{t("settings.duration-longBreak")}</div>
       <ClockInput value={longBreakTime} setValue={setLongBreakTime}/>
     </div>
   );
