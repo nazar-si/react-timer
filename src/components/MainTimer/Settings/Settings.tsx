f5165c2a8298a5da4f6f83b37edd7738445a6694
nazar-si <nazari61@yandex.ru>
2023-07-12 18:26:06 +0300

    feat: added settings labled

--- src/components/MainTimer/Settings/Settings.tsx
+++ src/components/MainTimer/Settings/Settings.tsx
@@ -14,8 +14,12 @@ export default function Settings({}: Props) {
   const [longBreakTime, setLongBreakTime] = useState(useTimerStore(s=>s.duration.longBreak));
   return (
     <div>
+      <div className='text-xl font-bold -mt-1'>Settings</div>
+      <div className='text-lg font-medium mt-3 -mb-8'>Focus duration:</div>
       <ClockInput value={focusTime} setValue={setFocusTime}/>
+      <div className='text-lg font-medium mt-3 -mb-8'>Break duration:</div>
       <ClockInput value={breakTime} setValue={setBreakTime}/>
+      <div className='text-lg font-medium mt-3 -mb-8'>Long break duration:</div>
       <ClockInput value={longBreakTime} setValue={setLongBreakTime}/>
     </div>
   );
