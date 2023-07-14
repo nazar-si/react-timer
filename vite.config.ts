b124b0069a2107feab84bf15cbe9a12c5a13aa7c
nazar-si <nazari61@yandex.ru>
2023-07-14 07:20:40 +0300

    fix: running pwa for testing

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,41 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [
     react(),
-    // VitePWA({}), // Temporary stoped PWA support
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        theme_color: '#f1f1f1',
+        background_color: '#f1f1f1',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        name: 'Pomodoro Timer',
+        short_name: 'Pomodoro',
+        description:
+          'Simple pomodoro timer for creating a more productive work environment',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
 });
