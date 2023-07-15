a19d8bd286548ea6ee745c442e947bfbcef79258
nazar-si <nazari61@yandex.ru>
2023-07-15 15:57:12 +0300

    fix: added title centring

--- src/components/MainTimer/Title/Title.tsx
+++ src/components/MainTimer/Title/Title.tsx
@@ -1,7 +1,7 @@
 export default function Title({ content }: { content: string }) {
   return (
     <div className="flex justify-center w-80 sm:w-96">
-      <h1 className="text-7xl sm:text-8xl lg:text-8xl mb-2 font-bold text-gray-200 dark:text-black/50 pointer-events-none select-none">
+      <h1 className="text-7xl sm:text-8xl text-center lg:text-8xl mb-2 font-bold text-gray-200 dark:text-black/50 pointer-events-none select-none">
         {content}
       </h1>
     </div>
