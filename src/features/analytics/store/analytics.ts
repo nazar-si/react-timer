f8764a07731b2f8f86e7b1624273828793ca1498
nazar-si <nazari61@yandex.ru>
2023-07-18 16:31:49 +0300

    feat: implemented analytics store

--- src/features/analytics/store/analytics.ts
+++ src/features/analytics/store/analytics.ts
@@ -1,23 +1,71 @@
 import { persist } from 'zustand/middleware';
 import { create } from 'zustand';
 
+export type modeType = 'focus' | 'break' | 'longBreak';
 export interface IEvent {
-  mode: 'focus' | 'break' | 'longBreak';
+  mode: modeType;
   startTime: Date;
   finishTime?: Date;
+  countedTime: number;
 }
 
 export interface IAnalyticsStore {
-  events: Array<IEvent>;
+  globalId: number;
+  events: Map<number, IEvent>;
+  dispatchEvent: (mode: modeType) => number;
+  addTimeToEvent: (eventId: number, time: number) => void;
+  stopEvent: (eventId: number) => void;
 }
 
 const useAnalyticsStore = create(
   persist<IAnalyticsStore>(
     (set, get) => ({
-      events: [],
+      events: new Map(),
+      globalId: 0,
+      dispatchEvent: (mode) => {
+        set({
+          events: new Map(
+            [...get().events.entries()].concat([
+              get().globalId + 1,
+              {
+                mode,
+                startTime: new Date(),
+                countedTime: 0,
+              },
+            ]),
+          ),
+          globalId: get().globalId + 1,
+        });
+        return get().globalId + 1;
+      },
+      addTimeToEvent: (eventId, time) => {
+        const e = get().events.get(eventId);
+        if (!e) return;
+        set({
+          events: new Map([
+            ...get().events.entries(),
+            [
+              eventId,
+              {
+                ...e,
+                countedTime: e.countedTime + time,
+              },
+            ],
+          ]),
+        });
+      },
+      stopEvent: (eventId) =>
+        set({
+          events: new Map(
+            [...get().events.entries()].filter(([id, _]) => id !== eventId),
+          ),
+        }),
     }),
     {
       name: 'analytics-store',
+      version: 1,
     },
   ),
 );
+
+export default useAnalyticsStore;
